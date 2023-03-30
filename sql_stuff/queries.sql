34f2bb57bdcdbf22135a046d1aec329ac1599b74
Alec Keeler <alec.keeler@gmail.com>
2023-03-30 16:33:49 -0500

    demo complete

--- sql_stuff/queries.sql
+++ sql_stuff/queries.sql
@@ -31,3 +31,32 @@ WHERE name = 'Onix';
 UPDATE pokemon
 SET height = height + 1000;
 -- WHERE
+
+SELECT * FROM pokemon
+WHERE name LIKE 'mil%e';
+
+SELECT * FROM pokemon
+WHERE rarity_id IN (2, 3, 5);
+
+SELECT * FROM pokemon
+WHERE height BETWEEN 0.5 AND 10;
+
+SELECT * FROM pokemon
+WHERE evolves = 1
+ORDER BY height DESC, name;
+
+SELECT * FROM pokemon
+ORDER BY name
+LIMIT 5
+OFFSET 10;
+
+SELECT pokemon.name, rarity.*
+FROM rarity
+JOIN pokemon ON (pokemon.rarity_id = rarity.id)
+WHERE rarity.value = 'common';
+
+SELECT trainers.name, trainers.id, pokemon_trainers.trainer_id, pokemon_trainers.pokemon_id, pokemon.id, pokemon.name
+FROM pokemon_trainers
+JOIN trainers ON (trainers.id = pokemon_trainers.trainer_id)
+JOIN pokemon ON (pokemon_trainers.pokemon_id = pokemon.id)
+WHERE trainers.name = 'Dan';
